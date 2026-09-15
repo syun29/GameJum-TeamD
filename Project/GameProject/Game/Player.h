37af4d0a0e8fbbf0652c53c801f83128f4f32068
matsushita03 <26051909-arch>
2026-09-15 14:45:25 +0900

    Player

--- Project/GameProject/Game/Player.h
+++ Project/GameProject/Game/Player.h
@@ -2,10 +2,38 @@
 #include"Base/Base.h"
 
 class Player : public Base {
-public:
+private:
+	//アニメーションの種類
+	enum {
+		eAnimIdle = 0,
+		eAnimRun,
+		eAnimJumpUp,
+		eAnimJumpDown,
+		eAnimDown,
+	};
+
+	//状態
+	enum {
+		eState_Idle,
+		eState_Down,
+	};
+
+	//各状態での挙動
+	void StateIdle();
+	void StateDown();
+
+	//状態変数
+	int m_state;
+	//着地フラグ
+	bool m_is_ground;
 	CImage m_img;
+
+
 public:
 	Player(const CVector2D& pos);
 	void Update();
 	void Draw();
+	void Collision(Base* b);
+
+	static TexAnimData _anim_data[];
 };
