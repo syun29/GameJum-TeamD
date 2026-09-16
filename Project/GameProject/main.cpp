3f650952593161593b9ec3a34707013be42a8351
matsushita03 <26051909-arch>
2026-09-16 12:15:15 +0900

    a

--- Project/GameProject/main.cpp
+++ Project/GameProject/main.cpp
@@ -105,11 +105,11 @@ void Init(void)
 	//松下
 
 	//画像の先読み込みと先登録　"リソース名"      "ファイル名"   アニメーションデータ, 幅、高さ
-	//ADD_RESOURCE("Player", CImage::CreateImage("Image/Player.png", Player::_anim_data, 16, 16));
+	ADD_RESOURCE("Player", CImage::CreateImage("Image/Player/Player.png", Player::_anim_data, 24, 24));
 
 
 
-	//Base::Add(new Player(CVector2D(0, 0)));
+	Base::Add(new Player(CVector2D(256, 540), false));
 
 
 
