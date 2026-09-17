e1ca200f49a71db569d611545e18c963c4f860c9
matsushita03 <26051909-arch>
2026-09-17 11:06:51 +0900

    a

---
+++ Project/GameProject/Game/UI.cpp
@@ -0,0 +1,29 @@
+#include"UI.h"
+#include"GameData.h"
+
+UI::UI() : Base(eType_UI)
+{
+	m_img = COPY_RESOURCE("UI", CImage);
+
+
+}
+
+void UI::Update()
+{
+
+}
+
+void UI::Draw()
+{
+    int time = GameData::s_time / 60;   //•b‚É•ÏŠ·
+
+    for (int i = 0; i < 3; i++, time /= 10) {
+        int t = time % 10;
+
+        m_img.SetRect(16 * t, 16, 16 * t + 16, 32);  //‰æ‘œ‚ÌØ‚è”²‚«
+        m_img.SetSize(64, 64);
+
+        m_img.SetPos(1600 - 64 * i, 50);  //‰Eã‚É•\Ž¦
+        m_img.Draw();
+    }
+}
---
+++ Project/GameProject/Game/UI.h
@@ -0,0 +1,13 @@
+#pragma once
+#include"../Base/Base.h"
+
+class UI : public Base
+{
+public:
+	CImage m_img;
+
+public:
+	UI();
+	void Update();
+	void Draw();
+};
