48ac0554fb26401f7ce1229e01387017773739f4
matsushita03 <26051909-arch>
2026-09-17 12:07:44 +0900

    a

--- Project/GameProject/Game/Player.cpp
+++ Project/GameProject/Game/Player.cpp
@@ -87,9 +87,7 @@ void Player::StateIdle()
 		}
 	}
 
-	if (!m_is_ground) {
-		m_cnt++;
-	}
+	
 
 	//地面にいるなら
 	else
@@ -166,6 +164,16 @@ void Player::Update()
 	//スクロール値設定
 	m_scroll.x = m_pos.x - 1920 / 2;
 	m_scroll.y = m_pos.y - 900;
+
+	if (!m_is_ground && m_pos.y < 250) {
+		SetKill();
+	}
+
+	if (!m_is_ground && m_pos.y > 1500) {
+		SetKill();
+	}
+
+
 }
 
 void Player::Draw() 
