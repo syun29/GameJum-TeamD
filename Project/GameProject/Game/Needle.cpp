ebc000252ca5a0933a04a715ec8833fd2eff4fc4
kawamura <26051903@d.anabuki-college.net>
2026-09-17 12:36:06 +0900

    a

--- Project/GameProject/Game/Needle.cpp
+++ Project/GameProject/Game/Needle.cpp
@@ -4,7 +4,7 @@ Needle::Needle(const CVector2D& pos,bool Flip) : Base(eType_Needle) {
 	m_img = COPY_RESOURCE("Needle", CImage);
 	m_pos = pos;	
 	m_img.SetSize(64*1.2, 64 * 1.2);
-	m_img.SetCenter(32 * 1.3, 64 * 1.3);
+	m_img.SetCenter(32 * 1.2, 64 * 1.2);
 	m_rect = CRect(-24 * 1.6, -48 * 1.6, 24 * 1.6, 0 * 1.6);
 	m_Flip = Flip;
 }
